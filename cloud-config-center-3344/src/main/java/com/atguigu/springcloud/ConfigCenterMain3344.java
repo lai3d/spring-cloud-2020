b9e5715097fa063be32fc45dc39c11df8aaac821
Larry Lai <cgpanda.sg@gmail.com>
2021-09-19 15:38:44 +0800

    Config配置总控中心搭建

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
+
+// test
+// http://config-3344.com:3344/master/config-dev.yml
