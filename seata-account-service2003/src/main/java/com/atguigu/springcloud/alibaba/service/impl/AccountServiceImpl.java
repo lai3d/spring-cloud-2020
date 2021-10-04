b4f80a7624583861687482a5f9a0c78074a90e0f
Larry Lai <cgpanda.sg@gmail.com>
2021-10-04 17:29:01 +0800

    Turn off exception to test seata distributed transaction

--- seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -30,8 +30,8 @@ public class AccountServiceImpl implements AccountService {
         LOGGER.info("------->account-service中扣减账户余额开始");
         //模拟超时异常，全局事务回滚
         //暂停几秒钟线程
-        try { TimeUnit.SECONDS.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
-        accountDao.decrease(userId,money);
+        //try { TimeUnit.SECONDS.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
+        accountDao.decrease(userId, money);
         LOGGER.info("------->account-service中扣减账户余额结束");
     }
 }
