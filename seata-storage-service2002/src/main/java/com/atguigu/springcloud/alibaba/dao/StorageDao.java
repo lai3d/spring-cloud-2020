5b1429ed56acd3f318bca057ee8521677e75c134
Larry Lai <cgpanda.sg@gmail.com>
2021-10-04 15:10:41 +0800

    Seata related service modules

--- seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/dao/StorageDao.java
+++ seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/dao/StorageDao.java
@@ -1,2 +1,11 @@
-package com.atguigu.springcloud.alibaba.dao;public interface StorageDao {
+package com.atguigu.springcloud.alibaba.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface StorageDao {
+
+    //扣减库存
+    void decrease(@Param("productId") Long productId, @Param("count") Integer count);
 }
